9c8741146812a9abc5df08d924cd7f49c49f28ff
Hossynkoala <Hossynassghary@gmail.com>
2020-11-16 23:25:06 +0330

    Start versin 2

--- Dashboard/Settings.cs
+++ Dashboard/Settings.cs
@@ -15,7 +15,7 @@
             //
             // this.SettingsSaving += this.SettingsSavingEventHandler;
             //
-        }
+        } 
         
         private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e) {
             // Add code to handle the SettingChangingEvent event here.
