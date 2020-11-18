88ad87d775e6813cc8730218b1ad7f714de4ed6a
Hossynkoala <Hossynassghary@gmail.com>
2020-11-18 17:45:34 +0330

    fix shop cheackpoint

--- Dashboard/Dashboards/Dashboard-Game/PageShop/PageShop.xaml.cs
+++ Dashboard/Dashboards/Dashboard-Game/PageShop/PageShop.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,70 @@ using System.Windows.Shapes;
 
 namespace Dashboard.Dashboards.Dashboard_Game.PageShop
 {
-    /// <summary>
-    /// Interaction logic for PageShop.xaml
-    /// </summary>
     public partial class PageShop : UserControl
     {
+        BsonDocument Detail = new BsonDocument
+        {
+            {"Name","" },
+            {"Description", ""},
+            {"MarketLink","" },
+            {"AvatarLink","" },
+            {"Tags",new BsonArray() },
+            {"Products" ,new BsonArray()},
+        };
+
         public PageShop()
         {
             InitializeComponent();
+
+
+            //acaton add Store
+            BTNaddStore.MouseDown += (s, e) =>
+            {
+                try
+                {
+
+                    if (TextboxName.Text.Length <= 5)
+                        throw new Exception("Store name Short");
+
+                    if (TextBoxMarketLink.Text.Length >= 1)
+                    {
+                        try
+                        {
+                            new Uri(TextBoxMarketLink.Text);
+
+                        }
+                        catch (Exception)
+                        {
+                            DashboardGame.Notifaction("The \" Market link \" is incorrect", Notifaction.StatusMessage.Error);
+                        }
+
+                    }
+
+
+                    if (TextBoxAvatar.Text.Length >= 1)
+                    {
+                        try
+                        {
+                            new Uri(TextBoxAvatar.Text);
+
+                        }
+                        catch (Exception ex)
+                        {
+                            DashboardGame.Notifaction("The \" Avatar link \" is incorrect", Notifaction.StatusMessage.Error);
+                        }
+
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+
+                    DashboardGame.Notifaction(ex.Message, Notifaction.StatusMessage.Error);
+                }
+
+            };
         }
     }
 }
