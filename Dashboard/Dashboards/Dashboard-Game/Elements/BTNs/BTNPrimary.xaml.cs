de89c45fc606f7e78f62986e472592b593289301
Hossynkoala <Hossynassghary@gmail.com>
2021-01-17 23:34:46 +0330

    Add design system Add BTNS Add Input

---
+++ Dashboard/Dashboards/Dashboard-Game/Elements/BTNs/BTNPrimary.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard.Dashboards.Dashboard_Game.Elements.BTNs
+{
+    /// <summary>
+    /// Interaction logic for BTNPrimary.xaml
+    /// </summary>
+    public partial class BTNPrimary : UserControl
+    {
+        public string NameText { get { return X.Text; } set { X.Text = value; } }
+
+        public BTNPrimary()
+        {
+            InitializeComponent();
+
+            X.Text = NameText;
+
+            GotFocus += (s, e) =>
+            {
+                BorderFocuse.BorderThickness = new Thickness(2);
+            };
+
+            LostFocus += (s, e) =>
+            {
+
+                BorderFocuse.BorderThickness = new Thickness(0);
+            };
+
+        }
+    }
+}
---
+++ Dashboard/Dashboards/Dashboard-Game/Elements/BTNs/SecondaryButtonIcon.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard.Dashboards.Dashboard_Game.Elements.BTNs
+{
+    /// <summary>
+    /// Interaction logic for SecondaryButtonIcon.xaml
+    /// </summary>
+    public partial class SecondaryButtonIcon : UserControl
+    {
+        public SecondaryButtonIcon()
+        {
+            InitializeComponent();
+
+            GotFocus += (s, e) =>
+            {
+                BorderFocuse.BorderThickness = new Thickness(2);
+            };
+
+            LostFocus += (s, e) =>
+            {
+                BorderFocuse.BorderThickness = new Thickness(0);
+            };
+        }
+    }
+}
