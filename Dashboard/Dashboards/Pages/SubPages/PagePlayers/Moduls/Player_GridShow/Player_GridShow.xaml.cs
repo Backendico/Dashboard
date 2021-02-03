56270d0859e65dad42b7ab8a90b3fa0c34b10e78
Hossynkoala <Hossynassghary@gmail.com>
2021-02-03 23:37:42 +0330

    cheackpoint

--- Dashboard/Dashboards/Pages/SubPages/PagePlayers/Moduls/Player_GridShow/Player_GridShow.xaml.cs
+++ Dashboard/Dashboards/Pages/SubPages/PagePlayers/Moduls/Player_GridShow/Player_GridShow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,36 @@ namespace Dashboard.Dashboards.Pages.SubPages.PagePlayers.Moduls.Player_GridShow
         public Player_GridShow()
         {
             InitializeComponent();
+
+            BTNMore.MouseDown += (s, e) =>
+            {
+                OpenMore();
+            };
+
+            MouseLeave += (s, e) => {
+                CloseMore();
+            };
+        }
+
+        void OpenMore()
+        {
+            DoubleAnimation Anim = new DoubleAnimation(1, TimeSpan.FromSeconds(0.3f));
+            Storyboard.SetTargetName(Anim, PanelMore.Name);
+            Storyboard.SetTargetProperty(Anim, new PropertyPath("Opacity"));
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(Anim);
+            storyboard.Begin(this);
+        }
+
+        void CloseMore()
+        {
+
+            DoubleAnimation Anim = new DoubleAnimation(0, TimeSpan.FromSeconds(0.3f));
+            Storyboard.SetTargetName(Anim, PanelMore.Name);
+            Storyboard.SetTargetProperty(Anim, new PropertyPath("Opacity"));
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(Anim);
+            storyboard.Begin(this);
         }
     }
 }
