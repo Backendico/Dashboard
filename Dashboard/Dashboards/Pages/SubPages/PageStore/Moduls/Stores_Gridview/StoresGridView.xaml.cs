575102b4e14eca0e54a4ffc030b21898488bf93c
Hossynkoala <Hossynassghary@gmail.com>
2021-02-10 23:40:51 +0330

    add gridview store

---
+++ Dashboard/Dashboards/Pages/SubPages/PageStore/Moduls/Stores_Gridview/StoresGridView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dashboard.Dashboards.Pages.SubPages.PageStore.Moduls.Stores_Gridview
+{
+    /// <summary>
+    /// Interaction logic for StoresGridView.xaml
+    /// </summary>
+    public partial class StoresGridView : UserControl
+    {
+        public StoresGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
